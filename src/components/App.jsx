89d202a87d038ec6a96d509730bd303be92415f1
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-06-13 12:19:43 +0300

    Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,89 @@
-import { ContactsForm } from './ContactsForm/ContactsForm';
-import { Filter } from './Filter/Filter';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts } from '../redux/Contacts/selectors';
-import ContactsList from './ContactsList/ContactsList';
+import React from 'react';
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+// import { ContactsForm } from './ContactsForm/ContactsForm';
+// import { Filter } from './Filter/Filter';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectContacts } from '../redux/Contacts/selectors';
+// import ContactsList from './ContactsList/ContactsList';
+// import { fetchContacts } from '../redux/Contacts/operations';
+
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Layout } from './Layout';
+
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from '../hooks/useAuth';
+
 import './App.css';
-import { useEffect } from 'react';
-import { fetchContacts } from '../redux/Contacts/operations';
+
+const HomePage = lazy(() => import('../pages/HomePage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export function App() {
-  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
+  // const contacts = useSelector(selectContacts);
+  // const dispatch = useDispatch();
 
-  return (
-    <div className="App__container">
-      <h1>Phonebook</h1>
-      <ContactsForm />
-      <h2>Contacts</h2>
-      <h3>Find contacts by name</h3>
-      {contacts.length > 0 ? (
-        <>
-          <Filter />
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
-          <ContactsList />
-        </>
-      ) : (
-        <p>No contacts</p>
-      )}
-    </div>
+  // return (
+  //   <div className="App__container">
+  //     <h1>Phonebook</h1>
+  //     <ContactsForm />
+  //     <h2>Contacts</h2>
+  //     <h3>Find contacts by name</h3>
+  //     {contacts.length > 0 ? (
+  //       <>
+  //         <Filter />
+
+  //         <ContactsList />
+  //       </>
+  //     ) : (
+  //       <p>No contacts</p>
+  //     )}
+  //   </div>
+  // );
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,13 @@
-// import { Helmet } from 'react-helmet';
-// import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { Helmet } from 'react-helmet';
+import { RegisterForm } from '../components/RegisterForm/RegisterForm';
 
-// export default function Register() {
-//   return (
-//     <div>
-//       <Helmet>
-//         <title>Registration</title>
-//       </Helmet>
-//       <RegisterForm />
-//     </div>
-//   );
-// }
+export default function Register() {
+  return (
+    <div>
+      <Helmet>
+        <title>Registration</title>
+      </Helmet>
+      <RegisterForm />
+    </div>
+  );
+}
